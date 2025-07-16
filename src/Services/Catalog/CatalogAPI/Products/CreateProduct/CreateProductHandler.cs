37ddd376eb3dedec25910beed77bf70eae4e4f07
Fanis Chamalidis <fchamalidis@yahoo.com>
2025-07-16 18:14:51 +0300

    Created Product model and initial CreateProduct "action" while introducing MediatR

---
+++ src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+
+namespace CatalogAPI.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+        : IRequest<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+
+    internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+    {
+        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            // Logic to create a product would go here
+            // For now, we will return a dummy result
+            var productId = Guid.NewGuid();
+            return Task.FromResult(new CreateProductResult(productId));
+        }
+    }
+}
